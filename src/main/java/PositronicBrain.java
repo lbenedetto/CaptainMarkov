25f00c681723d2b0f9c8944c88627d101df8eda0
Lars <larsbenedetto@gmail.com>
2016-02-03 14:04:59 -0800

    Made it easier to generate N lines of dialogue of a given type

--- src/main/java/PositronicBrain.java
+++ src/main/java/PositronicBrain.java
@@ -10,10 +10,16 @@ public class PositronicBrain {
 		for (int i = 0; i < characterNames.length; i++)
 			characters[i] = new MarkovChain(new Character(characterNames[i]));
 		//Generate a captains log
-		captainsLogs.generateSentence();
+		generate(10, captainsLogs);
 		//Generate a command to the computer
-		commands.generateSentence();
+		generate(5, commands);
 		//Generate a line of dialogue from a random character
-		characters[ThreadLocalRandom.current().nextInt(0, characterNames.length)].generateSentence();
+		generate(5, characters[ThreadLocalRandom.current().nextInt(0, characterNames.length)]);
+	}
+
+	public static void generate(int n, MarkovChain mc){
+		for(int i =0; i < n; i++){
+			mc.generateSentence();
+		}
 	}
 }
